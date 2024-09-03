d09300a079053588d9f9fe3ba44729d7d1e59678
unbengable12 <s15978565660@163.com>
2024-09-03 19:42:48 +0800

    前端注册功能 后端判断邮箱非空

--- src/main/java/com/example/entity/vo/request/EmailRegisterVo.java
+++ src/main/java/com/example/entity/vo/request/EmailRegisterVo.java
@@ -1,6 +1,8 @@
 package com.example.entity.vo.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -8,6 +10,7 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class EmailRegisterVo {
     @Email
+    @NotEmpty
     String email;
     @Length(min = 6, max = 6)
     String code;
